1f9ddf2fb85aa12e621ab279ce8ff002f9bed880
stonear <stonear@users.noreply.github.com>
2020-03-02 19:19:20 +0700

    Update ticket.blade.php

--- resources/views/mail/ticket.blade.php
+++ resources/views/mail/ticket.blade.php
@@ -1,12 +1,18 @@
 @component('mail::message')
-# Introduction
+# Kepada Yth. <b>{{ $user->name }}</b>.
 
-Hi {{ $user->name }}.
+Terima kasih atas partisipasinya dalam kegiatan {{ config('app.name') }}.
+
+Pengambilan <b>Seminar Kit</b> dapat dilakukan ditempat acara <i>Hotel Aria Gajayana Malang</i>, Sedangkan Pengambilan <b>Fun Run Racepack</b> dapat dilakukan di <i>BPSDM Prov Jatim, Jl. Kawi No 41 Malang</i> mulai tanggal <b>20 Maret 2020 pukul 13.00 WIB</b>.
+
+<b>NB:</b> <i>Setiap kegiatan harap menunjukkan e tiket kepada panitia.</i>
+
+Tiket dapat diunduh melalui lampiran email ini, atau melalui website {{ config('app.name') }}.
 
 {{-- @component('mail::button', ['url' => ''])
 Button Text
 @endcomponent --}}
 
-Thanks,<br>
-{{ config('app.name') }}
+Terima Kasih,<br/>
+<b>Panitia {{ config('app.name') }}</b>
 @endcomponent
